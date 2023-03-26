160064a97b17820c728269366feeeb09a8899cf8
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-03-26 20:47:51 +0200

    Dodate biblioteke za rad sa .env fajlovima i biblioteke za rad sa postgresql-om

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
+import psycopg2
 from flask import Flask
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
 
 
 @app.get("/")
