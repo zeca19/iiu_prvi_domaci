59658fcbb44ea9fdf4c611c9c799fd15014871fc
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-03-26 21:06:53 +0200

    Napravljena tabela za sobe i tabela za temperature i linkovane su obe tabele, na to je jos dodat upit za dodavanje podataka u te tabele

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import psycopg2
 from flask import Flask
 from dotenv import load_dotenv
 
+CREATE_ROOMS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+)
+CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL,
+                         date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
+
+
+INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
+
+INSERT_TEMP = (
+    "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
+)
+
 load_dotenv()
 
 app = Flask(__name__)
